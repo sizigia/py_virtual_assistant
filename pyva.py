95871394ba9c9c7408a3797913f443effecf9beb
sizigia <cumbresdigitales@gmail.com>
2019-10-20 13:53:14 -0300

    wrapped all the code with a while loop so it keeps running and asking what do I want to look up

--- pyva.py
+++ pyva.py
@@ -4,13 +4,12 @@ import wikipedia
 from dotenv import load_dotenv
 load_dotenv()
 
-raw_input = input("Whatcha wanna know: ")
-app_id = os.getenv('WOLFRAM_ID')
-client = wolframalpha.Client(app_id)
-
-result = client.query(raw_input)
-# answer1 = next(result.results).text
-answer2 = wikipedia.summary(raw_input)
-
-# print(answer1)
-print(answer2)
+while True:
+    raw_input = input("Whatcha wanna know: ")
+    app_id = os.getenv('WOLFRAM_ID')
+    client = wolframalpha.Client(app_id)
+    result = client.query(raw_input)
+    # answer1 = next(result.results).text
+    answer2 = wikipedia.summary(raw_input)
+    # print(answer1)
+    print(answer2)
