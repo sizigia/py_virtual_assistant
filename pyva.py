9c6e8f4087d54e2d2729126117e04502657dad4e
sizigia <cumbresdigitales@gmail.com>
2019-10-20 02:56:53 -0300

    (prev install/update python-dotenv) import only load_dotenv function and call it, this will read the -env file placed in the same folder as the project, and extract the ID

--- pyva.py
+++ pyva.py
@@ -1,5 +1,7 @@
 import os
 import wolframalpha
+from dotenv import load_dotenv
+load_dotenv()
 
 raw_input = input("Whatcha wanna know: ")
 app_id = os.getenv('WOLFRAM_ID')
@@ -7,3 +9,5 @@ client = wolframalpha.Client(app_id)
 
 result = client.query(raw_input)
 answer = next(result.results).text
+
+print(answer)
