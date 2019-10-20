9c04d7c7da0c9aa3d5d5129489e3c17d5d7deed5
sizigia <cumbresdigitales@gmail.com>
2019-10-20 18:26:46 -0300

    combined both API queries into the same infinite loop

--- pyva.py
+++ pyva.py
@@ -6,15 +6,20 @@ load_dotenv()
 
 while True:
     raw_input = input("Whatcha wanna know: ")
-    app_id = os.getenv('WOLFRAM_ID')
-    client = wolframalpha.Client(app_id)
-    result = client.query(raw_input)
-    # answer1 = next(result.results).text
-    lang = input("Which language do you feel comfortable with? ")[:2].upper()
-    if lang == "":
-        wikipedia.set_lang("ES")
-    else:
-        wikipedia.set_lang(lang)
-    answer2 = wikipedia.summary(raw_input, sentences=3)
-    # print(answer1)
-    print(answer2)
+    try:
+        # wolframalpha
+        app_id = os.getenv('WOLFRAM_ID')
+        client = wolframalpha.Client(app_id)
+        result = client.query(raw_input)
+        answer1 = next(result.results).text
+        print(answer1, '\n')
+    except pass:
+        # wikipedia
+        lang = input("Which language do you feel comfortable with? ")[
+            :2].upper()
+        if lang == "":
+            wikipedia.set_lang("ES")
+        else:
+            wikipedia.set_lang(lang)
+        answer2 = wikipedia.summary(raw_input, sentences=3)
+        print(answer2, '\n')
